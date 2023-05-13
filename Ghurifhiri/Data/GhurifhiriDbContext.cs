3b564b673483bd3b89704c9743111124ad69d640
Diganto <digantobhowmik@gmail.com>
2023-05-13 17:08:38 +0600

    db update

---
+++ Ghurifhiri/Data/GhurifhiriDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Ghurifhiri.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ghurifhiri.Data
+{
+	public class GhurifhiriDbContext : DbContext
+	{
+		public GhurifhiriDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<User> Users { get; set; }
+	}
+}
+
